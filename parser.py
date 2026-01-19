6a95b7aa65de22a5fe7f0e652bb6c77ce0c8c17a
Debra Ritter <zeeritter@gmail.com>
2026-01-19 13:39:14 -0500

    Created `parser.py` to get info from GitHub

---
+++ parser.py
@@ -0,0 +1,25 @@
+import requests # For getting info from github
+
+# GitHub API needs a user agent
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
+}
+
+# This can be any name, it needs to be sanitized later.
+# The username "torvalds" (for Linus Torvalds) is a good first example
+username = input("Input github username: ")
+
+# The URL to get a user's github event info
+user_event_str = f"https://api.github.com/users/{username}/events"
+
+# Get the actual info from github
+response = requests.get(user_event_str)
+response.raise_for_status() # In case we get a bad request
+
+events_data = response.json()
+
+# Print first 5 events
+for e in events_data[:5]:
+    event_type = e.get('type', 'Unknown')
+    repo_name = e.get('repo', {}).get('name', 'Unknown')
+    print(f"{event_type} in {repo_name}")
